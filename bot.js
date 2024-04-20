8d955e43c9aca46e39156a377b338dc36c87099e
yur0n <yur0n@yahoo.com>
2024-04-20 09:48:54 +0300

    up

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ import { Bot, GrammyError, HttpError } from 'grammy';
 import User from './db.js';
 const bot = new Bot(process.env.BOT);
 
-bot.command('start', ctx => ctx.reply('Привет, я бот для отслеживания посылок!\n\nОтправьте мне свой номер телефона в формате +79123456789 или +380123456789 и я вам сообщу, когда и куда прибудет ваша посылка!'));
+bot.command('start', ctx => ctx.reply('Привет, я бот для отслеживания посылок компании YES !\nОтправьте мне свой номер телефона в формате +79123456789 или +380123456789 и я вам сообщу, когда и куда прибудет ваша посылка!'));
 bot.on('message', async ctx => {
 	if (ctx.msg.text.match(/^\+79\d{9}$/) || ctx.msg.text.match(/^\+380\d{9}$/)) {
 		try {
@@ -12,7 +12,7 @@ bot.on('message', async ctx => {
 			} else {
 				let newUser = new User({ chat_id: ctx.from.id, phone: ctx.msg.text });
 				await newUser.save();
-				ctx.reply('Спасибо, ждите уведомления о статусе вашей посылки!');
+				ctx.reply('Спасибо, ждите уведомлений о прибытии ваших заказов!');
 			}
 		}
 		catch (e) {
@@ -20,7 +20,7 @@ bot.on('message', async ctx => {
 			ctx.reply('Произошла ошибка, попробуйте позже');
 		}
 	} else {
-		ctx.reply('Мне нужен только ваш номер телефона в формате +79123456789 или +380123456789');
+		ctx.reply('Я предназначен только для уведомлений о прибытии ваших заказов. Все, что мне нужно это ваш номер телефона в формате +79123456789 или +380123456789');
 	}
 });
 
